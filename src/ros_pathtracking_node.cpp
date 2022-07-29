3149b1e7f52d38b1c11a7a11f5c9d1a5b7a82144
lookatme <784817297@qq.com>
2022-07-29 19:05:44 +0800

    first commit

---
+++ src/ros_pathtracking_node.cpp
@@ -0,0 +1,28 @@
+#include <ros/ros.h>
+#include <signal.h>
+#include <cmath>
+
+#include "yaml-cpp/yaml.h"
+
+
+using namespace std;
+
+
+
+void mySigIntHandler(int sig)
+{
+    ROS_INFO("close ros_pathtracking!\r\n");
+    ros::shutdown();
+}
+
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc,argv,"ros_pathtracking",ros::init_options::NoSigintHandler); 
+    ros::NodeHandle nh;
+    signal(SIGINT, mySigIntHandler);
+
+    ros::spin();
+    exit(0) ;
+}
